3a36f8ef74b49e7ce87eddffd681a13401fe15fa
Rajiv786-ekaaya <rajiv@ekaaya.in>
2025-01-27 16:31:26 +0530

    Update config.js

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:5000";
+const URL = "https://webrtcs.onrender.com";
 // const URL = "https://video-call-server-gm7i.onrender.com";
 
 export const socket = io(URL);
